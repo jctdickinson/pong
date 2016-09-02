356584235f664e3f8c752bc004ecd6523333528f
jctdickinson <jctdickinson@gmail.com>
2016-09-01 21:40:52 -0700

    Added ball motion functions

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty,\
+    ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
 
 
 class PongBall(Widget):
@@ -10,12 +13,26 @@ class PongBall(Widget):
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
     def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+        self.pos += Vector(*self.velocity)
 
 
 class PongGame(Widget):
-    pass
+    ball = ObjectProperty(None)
 
+    def serve_ball(self):
+        self.ball.center = self.center
+        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+
+    def update(self, dt):
+        self.ball.move()
+
+        #bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.right > self.height):
+            self.ball.velocity_y *= -1
+
+        #bounce off left and right
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_y *= -1
 
 class PongApp(App):
     def build(self):
